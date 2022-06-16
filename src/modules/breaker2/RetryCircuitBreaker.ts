df6f8641f1101b22341b7aea9a9635ad5a34f14c
Fernando Acosta <lacosta@gobdigital.com>
2022-06-16 15:01:16 -0500

    Se discrimina el error por circuito abierto

--- src/modules/breaker2/RetryCircuitBreaker.ts
+++ src/modules/breaker2/RetryCircuitBreaker.ts
@@ -1,6 +1,7 @@
 import CircuitBreaker from 'opossum'
 import { delay } from '../../helpers/datetime'
 import logger from '../../helpers/logger'
+import clc from 'cli-color'
 
 interface Options extends CircuitBreaker.Options {
   maxRetries: number
@@ -13,19 +14,18 @@ export class RetryCircuitBraker<TI extends unknown[] = unknown[], TR = unknown> 
   private options: Options
 
   private retryCount = 0
-  private delayMillis = 2000
+  private delayMillis = 1000
 
   constructor(action: (...args: TI) => Promise<TR>, options: Options) {
     this.circuitBreaker = new CircuitBreaker(action, options)
     this.options = options
     this.options.name = this.options.name || ''
 
-    this.circuitBreaker.on('open', () => logger.info(`Breaker ${this.options.name} is Open`))
-    this.circuitBreaker.on('halfOpen', () => logger.info(`Breaker ${this.options.name} is Half Open`))
+    this.circuitBreaker.on('open', () => logger.info(clc.red(`[${this.options.name}] Breaker is Open`)))
+    this.circuitBreaker.on('halfOpen', () => logger.info(clc.yellow(`[${this.options.name}] Breaker is Half Open`)))
     this.circuitBreaker.on('close', () => {
-      logger.info(`Breaker ${this.options.name} is Close`)
-      this.delayMillis = 2000
-      this.retryCount = 0
+      logger.info(clc.green(`[${this.options.name}] Breaker is Close`))
+      this.resetDelayAndTryCount()
     })
   }
 
@@ -39,18 +39,29 @@ export class RetryCircuitBraker<TI extends unknown[] = unknown[], TR = unknown> 
       const response = await this.circuitBreaker.fire(...args)
       return response
     } catch (error: any) {
-      logger.warn(`Retrying Action due to ${error.message}. Waiting ${this.delayMillis} millis`)
+      logger.warn(`[${this.options.name}] Retrying Action due to ${error.message}. Waiting ${this.delayMillis} millis`)
 
-      if (!this.circuitBreaker.opened) {
-        this.retryCount += 1
-
-        this.delayMillis =
-          this.delayMillis * 2 < this.options.maxTimeWait ? (this.delayMillis *= 2) : this.options.maxTimeWait
+      if (error.code != 'EOPENBREAKER') {
+        this.incrementTryCount()
+        this.duplicateDelay()
       }
 
       await delay(this.delayMillis)
-
       return await this.tryAction(...args)
     }
+  }
+
+  private incrementTryCount() {
+    this.retryCount += 1
+  }
+
+  private duplicateDelay() {
+    this.delayMillis =
+      this.delayMillis * 2 < this.options.maxTimeWait ? (this.delayMillis *= 2) : this.options.maxTimeWait
+  }
+
+  private resetDelayAndTryCount() {
+    this.retryCount = 0
+    this.delayMillis = 1000
   }
 }
--- src/modules/breaker2/test.ts
+++ src/modules/breaker2/test.ts
@@ -12,18 +12,20 @@ async function asyncFunctionThatCouldFail(url: string) {
   return response.data
 }
 
-for (let i = 0; i < 3; i++) {
-  const retryCircuit = new RetryCircuitBraker(asyncFunctionThatCouldFail, {
-    timeout: 6000,
-    errorThresholdPercentage: 50,
-    resetTimeout: 8000,
-    maxRetries: 3,
-    maxTimeWait: 10000,
-    name: `${i}`,
-  })
+for (let k = 0; k < 5; k++) {
+  for (let i = 0; i < 3; i++) {
+    const retryCircuit = new RetryCircuitBraker(asyncFunctionThatCouldFail, {
+      timeout: 6000,
+      errorThresholdPercentage: 50,
+      resetTimeout: 12000 + i * 2,
+      maxRetries: 3,
+      maxTimeWait: 10000,
+      name: `${i}`,
+    })
 
-  retryCircuit
-    .tryAction(servers[i])
-    .then((a) => console.log(`La respesta: ${a}`))
-    .catch((e) => logger.error(e.message))
+    retryCircuit
+      .tryAction(servers[i])
+      .then((a) => console.log(`La respesta: ${a}`))
+      .catch((e) => logger.error(e.message))
+  }
 }
